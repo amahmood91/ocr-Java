b2abaf871b326792e8097d0b6291715ba8433c44
Ali Mahmood <ali.mahmood91@gmail.com>
2016-06-19 17:16:46 -0400

    Adding local tesseract4j setup

---
+++ tesseractTest/src/tesseracttest/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesseracttest;
+import java.io.*;
+import net.sourceforge.tess4j.*;
+/**
+ *
+ * @author Ali
+ */
+ 
+public class Main {
+public static void main(String[] args) {
+File imageFile = new File("C:\\Users\\Ali\\Desktop\\testimg1.png");
+Tesseract instance = Tesseract.getInstance(); //
+ 
+try {
+ 
+String result = instance.doOCR(imageFile);
+System.out.println("Printing OCR output from image file:\n"+result);
+ 
+} catch (TesseractException e) {
+System.err.println(e.getMessage());
+}
+}
+}
